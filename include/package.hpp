669c9f4ce5fa6dae99991d44ab7386091673baeb
Paweł Lato <pawellato@student.agh.edu.pl>
2023-12-05 20:55:39 +0100

    zaimplementowano klase Package

--- include/package.hpp
+++ include/package.hpp
@@ -1,4 +1,27 @@
 #ifndef NETSIM_PACKAGE_HPP
 #define NETSIM_PACKAGE_HPP
 
+#include "types.hpp"
+#include <set>
+
+class Package{
+public:
+    Package();
+
+    Package(ElementID id) : id_(id) {assigned_IDs.insert(id);}
+
+    Package(Package &&) ;
+
+    Package &operator=(Package &&);
+
+    ElementID get_id() const { return id_; }
+
+    ~Package();
+
+private:
+    ElementID id_;
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;
+
+};
 #endif //NETSIM_PACKAGE_HPP
--- src/package.cpp
+++ src/package.cpp
@@ -1,1 +1,42 @@
 #include "package.hpp"
+
+std::set<ElementID> Package::assigned_IDs = {};
+std::set<ElementID> Package::freed_IDs = {};
+
+Package::Package() {
+    ElementID new_id;
+    if(!freed_IDs.empty()){
+        new_id = *freed_IDs.begin();
+        freed_IDs.erase(new_id);
+    }
+    else{
+        new_id = *assigned_IDs.end() + 1;
+    }
+    assigned_IDs.insert(new_id);
+    id_ = new_id;
+}
+
+Package::Package(Package &&p){
+    id_ = p.id_;
+    p.id_ = -1;
+}
+
+
+Package &Package::operator=(Package &&p){
+    if(id_ != -1){
+        freed_IDs.insert(id_);
+        assigned_IDs.erase(id_);
+        return *this;
+    }
+    id_ = p.id_;
+    p.id_ = -1;
+    return *this;
+}
+
+
+Package::~Package() {
+    if(id_ != -1){
+        freed_IDs.insert(id_);
+        assigned_IDs.erase(id_);
+    }
+}
