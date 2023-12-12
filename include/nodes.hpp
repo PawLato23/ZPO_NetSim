4d2d67afcb0083e4d739badadc15807658b290a5
Szymon Lipkowski <lipesszymon@interia.pl>
2023-12-12 15:53:24 +0100

    nodes

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -1,1 +1,98 @@
+#ifndef NETSIM_NODES_HPP
+#define NETSIM_NODES_HPP
+
+#include "package.hpp"
+#include "config.hpp"
+#include "types.hpp"
+#include "storage_types.hpp"
+#include "helpers.hpp"
+#include <memory>
+#include <map>
+#include <optional>
+#include <utility>
+
+class IPackageReceiver{
+public:
+    virtual void receive_package(Package &&p)=0;
+    virtual ElementID get_id() const=0;
+    virtual IPackageStockpile::const_iterator cbegin() const=0;
+    virtual IPackageStockpile::const_iterator begin() const=0;
+    virtual IPackageStockpile::const_iterator cend() const=0;
+    virtual IPackageStockpile::const_iterator end() const=0;
+    ~IPackageReceiver()=default;
+};
+class ReceiverPreferences{
+public:
+    using preferences_t = std::map<IPackageReceiver *, double>;
+    using const_iterator = preferences_t::const_iterator;
+    ReceiverPreferences(ProbabilityGenerator pg = probability_generator) : pg_(std::move(pg)) {}
+    const_iterator cbegin() const {return preferences_.cbegin();}
+    const_iterator begin() const {return preferences_.cbegin();}
+    const_iterator cend() const {return preferences_.cend();}
+    const_iterator end() const {return preferences_.cend();}
+    void add_receiver(IPackageReceiver *r);
+    void remove_receiver(IPackageReceiver *r);
+    IPackageReceiver *choose_receiver(void);
+    const preferences_t &get_preferences(void) const {return preferences_;}
+private:
+    preferences_t preferences_;
+    ProbabilityGenerator pg_;
+};
+class PackageSender{
+public:
+    PackageSender(PackageSender &&package_sender)=default;
+    void send_package();
+    const std::optional<Package> &get_sending_buffer() const {return bufor_;}
+protected:
+    void push_package(Package &&package) {bufor_.emplace(package.get_id());}
+    std::optional<Package> bufor_=std::nullopt;
+private:
+    ReceiverPreferences receiver_preferences_;
+};
+class Ramp : public PackageSender{
+public:
+    Ramp(ElementID id, TimeOffset di) : PackageSender(),id_(id),di_(di){}
+    void deliver_goods(Time t);
+    TimeOffset get_delivery_interval() const {return di_;}
+    ElementID get_id() const {return id_;}
+private:
+    ElementID id_;
+    TimeOffset di_;
+    Time t_;
+    std::optional<Package> bufor_=std::nullopt;
+};
+class Worker : public IPackageQueue, public PackageSender{
+public:
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q) : PackageSender(), id_(id), pd_(pd), q_(std::move(q)) {}
+    void do_work(Time t);
+    TimeOffset get_processing_duration() const {return pd_;}
+    Time get_package_processing_start_time() const{ return t_;}
+    void receive_package(Package &&p) override;
+    ElementID get_id() const override {return id_;}
+    IPackageStockpile::const_iterator cbegin() const override { return q_->cbegin(); }
+    IPackageStockpile::const_iterator cend() const override { return q_->cend(); }
+    IPackageStockpile::const_iterator begin() const override { return q_->begin(); }
+    IPackageStockpile::const_iterator end() const override { return q_->end(); }
+
+private:
+    ElementID id_;
+    TimeOffset pd_;
+    Time t_;
+    std::unique_ptr<IPackageQueue> q_;
+    std::optional<Package> bufor_=std::nullopt;
+};
+class Storehouse : public IPackageStockpile, public IPackageReceiver{
+public:
+    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d = std::make_unique<PackageQueue>(PackageQueueType::FIFO)) : id_(id), d_(std::move(d)) {}
+    void receive_package(Package &&p) override;
+    ElementID get_id() const override {return id_;}
+    IPackageStockpile::const_iterator cbegin() const override { return d_->cbegin(); }
+    IPackageStockpile::const_iterator cend() const override { return d_->cend(); }
+    IPackageStockpile::const_iterator begin() const override { return d_->begin(); }
+    IPackageStockpile::const_iterator end() const override { return d_->end(); }
+private:
+    ElementID id_;
+    std::unique_ptr<IPackageStockpile> d_;
+};
+#endif //NETSIM_NODES_HPP
 
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,1 +1,1 @@
-
+#include "nodes.hpp"
