ca5889af0ca30c815952f3a391dc1f011db1ca02
Adam-Lek <129278265+Adam-Lek@users.noreply.github.com>
2023-12-13 22:09:41 +0100

    dodanie ręcznych generatorów

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -10,6 +10,9 @@ extern std::random_device rd;
 extern std::mt19937 rng;
 
 extern double default_probability_generator();
+extern double single_vector_generator();
+extern double repeat_vector_generator();
+extern double single_value_generator();
 
 extern ProbabilityGenerator probability_generator;
 
