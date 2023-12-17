c32ad6a932796162e0143ddc7544b478449787ce
Paweł Lato <pawellato@student.agh.edu.pl>
2023-12-17 15:36:45 +0100

    dodano konstruktory przenoszace i wirtualne destruktory

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -11,6 +11,10 @@
 #include <optional>
 #include <utility>
 
+enum class ReceiverType {
+    WORKER, STOREHOUSE
+};
+
 class IPackageReceiver{
 public:
     virtual void receive_package(Package &&p)=0;
@@ -19,7 +23,10 @@ public:
     virtual IPackageStockpile::const_iterator begin() const=0;
     virtual IPackageStockpile::const_iterator cend() const=0;
     virtual IPackageStockpile::const_iterator end() const=0;
-    ~IPackageReceiver()=default;
+    #if (defined EXERCISE_ID && EXERCISE_ID != EXERCISE_ID_NODES)
+    virtual ReceiverType get_receiver_type() const = 0;
+    #endif
+    virtual ~IPackageReceiver()=default;
 };
 class ReceiverPreferences{
 public:
@@ -43,9 +50,11 @@ class PackageSender{
 public:
     PackageSender() = default;
     PackageSender(PackageSender &&package_sender)=default;
+    PackageSender& operator=(PackageSender&&) = default;
     void send_package();
     const std::optional<Package> &get_sending_buffer() const {return bufor_;}
     ReceiverPreferences receiver_preferences_;
+    virtual ~PackageSender() = default;
 protected:
     void push_package(Package &&package) {bufor_.emplace(package.get_id());}
     std::optional<Package> bufor_=std::nullopt;
@@ -54,6 +63,8 @@ protected:
 class Ramp : public PackageSender{
 public:
     Ramp(ElementID id, TimeOffset di) : PackageSender(),id_(id),di_(di){}
+    Ramp(Ramp&&) = default;
+    Ramp& operator=(Ramp&&) = default;
     void deliver_goods(Time t);
     TimeOffset get_delivery_interval() const {return di_;}
     ElementID get_id() const {return id_;}
@@ -66,6 +77,8 @@ private:
 class Worker : public IPackageReceiver, public PackageSender{
 public:
     Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q) : PackageSender(), id_(id), pd_(pd), q_(std::move(q)) {}
+    Worker(Worker&&) = default;
+    Worker& operator=(Worker&&) = default;
     void do_work(Time t);
     TimeOffset get_processing_duration() const {return pd_;}
     Time get_package_processing_start_time() const{ return t_;}
@@ -75,7 +88,9 @@ public:
     IPackageStockpile::const_iterator cend() const override { return q_->cend(); }
     IPackageStockpile::const_iterator begin() const override { return q_->begin(); }
     IPackageStockpile::const_iterator end() const override { return q_->end(); }
-
+    #if (defined EXERCISE_ID && EXERCISE_ID != EXERCISE_ID_NODES)
+    ReceiverType get_receiver_type() const override { return ReceiverType::WORKER; }
+    #endif
 private:
     ElementID id_;
     TimeOffset pd_;
@@ -86,12 +101,17 @@ private:
 class Storehouse : public IPackageReceiver{
 public:
     Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d = std::make_unique<PackageQueue>(PackageQueueType::FIFO)) : id_(id), d_(std::move(d)) {}
+    Storehouse(Storehouse&&) = default;
+    Storehouse& operator=(Storehouse&&) = default;
     void receive_package(Package &&p) override;
     ElementID get_id() const override {return id_;}
     IPackageStockpile::const_iterator cbegin() const override { return d_->cbegin(); }
     IPackageStockpile::const_iterator cend() const override { return d_->cend(); }
     IPackageStockpile::const_iterator begin() const override { return d_->begin(); }
     IPackageStockpile::const_iterator end() const override { return d_->end(); }
+    #if (defined EXERCISE_ID && EXERCISE_ID != EXERCISE_ID_NODES)
+    ReceiverType get_receiver_type() const override { return ReceiverType::STOREHOUSE; }
+    #endif
 private:
     ElementID id_;
     std::unique_ptr<IPackageStockpile> d_;
