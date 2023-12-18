89fe73e5541675c2bf98520292ea338487bbb091
Adam-Lek <129278265+Adam-Lek@users.noreply.github.com>
2023-12-18 21:47:14 +0100

    w include cały opis

--- src/factory.cpp
+++ src/factory.cpp
@@ -1,1 +1,10 @@
 #include "factory.hpp"
+
+void Factory::do_package_passing() {
+    for(auto& worker : _Worker_collection){
+        worker.send_package();
+    }
+    for(auto& ramp : _Ramp_collection){
+        ramp.send_package();
+    }
+}
