6664dc80fb3f5bed29d622b2e6e06cdf54ed661e
Adam-Lek <129278265+Adam-Lek@users.noreply.github.com>
2023-12-18 19:15:56 +0100

    Dodałem trochę do factory, przesunąłem "public:"
    dodałem metody ogólne dla naszych 3 klas oraz ogl zarys innych w pliku nagłowkowym. W klasie NodeCollection przesunąłem "public:" przed iteratory, ponieważ, nie byłem w stanie ich użyć.

--- include/factory.hpp
+++ include/factory.hpp
@@ -5,10 +5,11 @@
 #include <memory>
 
 template <typename Node> class NodeCollection{
+public: //przesunąłem "public:"
     using container_t = typename std::vector<Node>;
     using iterator = typename container_t::iterator;
     using const_iterator = typename container_t::const_iterator;
-public:
+//public:
     void add(Node&& node) {node_vector_.push_back(std::move(node));}
 
     iterator find_by_id(ElementID id){
@@ -42,7 +43,39 @@ private:
 };
 
 class Factory{
-    
+        //Ramp related methods
+    void add_ramp(Ramp&& new_ramp) {_Ramp_collection.add(std::move(new_ramp));}
+    void remove_ramp(ElementID id) {_Ramp_collection.remove_by_id(id);};
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) {return _Ramp_collection.find_by_id(id);}
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const {return _Ramp_collection.find_by_id(id);}
+    NodeCollection<Ramp>::const_iterator ramp_cbegin() const {return _Ramp_collection.cbegin();}
+    NodeCollection<Ramp>::const_iterator ramp_cend() const {return _Ramp_collection.cend();}
+        //Worker related methods
+    void add_worker(Worker&& new_worker) {_Worker_collection.add(std::move(new_worker));}
+    void remove_worker(ElementID id) {_Worker_collection.remove_by_id(id);};
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return _Worker_collection.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return _Worker_collection.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator worker_cbegin() const {return _Worker_collection.cbegin();}
+    NodeCollection<Worker>::const_iterator worker_cend() const {return _Worker_collection.cend();}
+        //Storehouse related methods
+    void add_storehouse(Storehouse&& new_storehouse) {_Storehouse_collection.add(std::move(new_storehouse));}
+    void remove_storehouse(ElementID id) {_Storehouse_collection.remove_by_id(id);};
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return _Storehouse_collection.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return _Storehouse_collection.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const {return _Storehouse_collection.cbegin();}
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() const {return _Storehouse_collection.cend();}
+        //Whole factory methods
+    bool is_consistent();
+    void do_deliveries(Time t);
+    void do_package_passing();
+    void do_work(Time t);
+
+private:
+    template<typename Node> //Nie jestem tego pewny, czy to czegoś nie wywali
+    void remove_reciver(NodeCollection<Node>& collection, ElementID id);
+    NodeCollection<Ramp> _Ramp_collection;
+    NodeCollection<Worker> _Worker_collection;
+    NodeCollection<Storehouse> _Storehouse_collection;
 };
 
 #endif //NETSIM_FACTORY_HPP
