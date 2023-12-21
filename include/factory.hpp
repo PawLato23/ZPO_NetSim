d62f910589ef1105df3724b0acd7a1ecb5aab212
Szymon Lipkowski <lipesszymon@interia.pl>
2023-12-21 18:57:40 +0100

    funkcje

--- include/factory.hpp
+++ include/factory.hpp
@@ -5,11 +5,10 @@
 #include <memory>
 
 template <typename Node> class NodeCollection{
-public: //przesunąłem "public:"
+public:
     using container_t = typename std::vector<Node>;
     using iterator = typename container_t::iterator;
     using const_iterator = typename container_t::const_iterator;
-//public:
     void add(Node&& node) {node_vector_.push_back(std::move(node));}
 
     iterator find_by_id(ElementID id){
@@ -53,14 +52,14 @@ public:
     NodeCollection<Ramp>::const_iterator ramp_cend() const {return _Ramp_collection.cend();}
         //Worker related methods
     void add_worker(Worker&& new_worker) {_Worker_collection.add(std::move(new_worker));}
-    void remove_worker(ElementID id) {_Worker_collection.remove_by_id(id);};
+    void remove_worker(ElementID id) { remove_receiver<Worker>(_Worker_collection,id);};
     NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return _Worker_collection.find_by_id(id);}
     NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return _Worker_collection.find_by_id(id);}
     NodeCollection<Worker>::const_iterator worker_cbegin() const {return _Worker_collection.cbegin();}
     NodeCollection<Worker>::const_iterator worker_cend() const {return _Worker_collection.cend();}
         //Storehouse related methods
     void add_storehouse(Storehouse&& new_storehouse) {_Storehouse_collection.add(std::move(new_storehouse));}
-    void remove_storehouse(ElementID id) {_Storehouse_collection.remove_by_id(id);};
+    void remove_storehouse(ElementID id) { remove_receiver<Storehouse>(_Storehouse_collection,id);};
     NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return _Storehouse_collection.find_by_id(id);}
     NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return _Storehouse_collection.find_by_id(id);}
     NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const {return _Storehouse_collection.cbegin();}
@@ -72,8 +71,16 @@ public:
     void do_work(Time t) {for(auto& worker : _Worker_collection){worker.do_work(t);}};
 
 private:
-    template<typename Node> //Nie jestem tego pewny, czy to czegoś nie wywali
-    void remove_reciver(NodeCollection<Node>& collection, ElementID id);
+    template<typename Node>
+    void remove_receiver(NodeCollection<Node>& collection, ElementID id){
+        collection.remove_by_id(id);
+        for(auto &ramp : _Ramp_collection){
+            ramp.receiver_preferences_.remove_receiver(&(*collection.find_by_id(id)));
+        }
+        for(auto &worker : _Worker_collection){
+            worker.receiver_preferences_.remove_receiver(&(*collection.find_by_id(id)));
+        }
+    }
     NodeCollection<Ramp> _Ramp_collection;
     NodeCollection<Worker> _Worker_collection;
     NodeCollection<Storehouse> _Storehouse_collection;
--- src/factory.cpp
+++ src/factory.cpp
@@ -8,6 +8,68 @@ void Factory::do_package_passing() {
         ramp.send_package();
     }
 }
+enum Color { NIEODWIEDZONY, ODWIEDZONY, ZWERYFIKOWANY };
+
+bool is_storehouse_available(PackageSender* send, std::map<PackageSender*,Color>& map){
+    auto it = map.find(send);
+    if (it->second == Color::ZWERYFIKOWANY){
+        return true;
+    }
+    it->second = Color::ODWIEDZONY;
+    if(send->receiver_preferences_.get_preferences().empty()){
+        return false;
+    }
+    bool different_send=false;
+    for(auto pair : send->receiver_preferences_.get_preferences()){
+        if(pair.first->get_receiver_type() == ReceiverType::STOREHOUSE){
+            different_send=true;
+        }
+        else if(pair.first->get_receiver_type() == ReceiverType::WORKER){
+            IPackageReceiver* receiver_ptr = pair.first;
+            auto worker_ptr = dynamic_cast<Worker*>(receiver_ptr);
+            auto sendrecv_ptr = dynamic_cast<PackageSender*>(worker_ptr);
+            if (sendrecv_ptr != send){
+                different_send = true;
+            }
+            if(map.find(sendrecv_ptr)->second == Color::NIEODWIEDZONY){
+                bool is = is_storehouse_available(sendrecv_ptr,map);
+                if(!is){
+                    return false;
+                }
+            }
+        }
+    }
+    it->second=Color::ZWERYFIKOWANY;
+    if(different_send){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool Factory::is_consistent(){
+    std::map<PackageSender*,Color> color_collection;
+    for(auto &ramp : _Ramp_collection){
+        color_collection[&ramp]=Color::NIEODWIEDZONY;
+    }
+    for(auto &worker : _Worker_collection){
+        color_collection[&worker]=Color::NIEODWIEDZONY;
+    }
+    for(auto &ramp : _Ramp_collection){
+        bool is = is_storehouse_available(&ramp, color_collection);
+        if(!is){
+            return false;
+        }
+    }
+    for(auto &worker : _Worker_collection){
+        bool is = is_storehouse_available(&worker, color_collection);
+        if(!is){
+            return false;
+        }
+    }
+    return true;
+}
 
 Factory load_factory_structure(std::istream& is){
 
