ed87a99ee6c5e6d43c2e0c569c7c1a460277d435
Paweł Lato <pawellato@student.agh.edu.pl>
2023-12-28 20:09:42 +0100

    zaimplementowano funkcje get_processing_buffer

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -92,7 +92,7 @@ public:
     ReceiverType get_receiver_type() const override { return ReceiverType::WORKER; }
     #endif
     IPackageQueue* get_queue() const { return q_.get();}
-    std::optional<Package>& get_processing_buffer() const {};
+    const std::optional<Package>& get_processing_buffer() const {return bufor_;};
 private:
     ElementID id_;
     TimeOffset pd_;
