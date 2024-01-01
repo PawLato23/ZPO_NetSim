5bfabec4cf60658ec708a5290f1102c4084c535d
Paweł Lato <pawellato@student.agh.edu.pl>
2024-01-01 22:56:56 +0100

    poprawiono funkcje simulation

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,11 +1,11 @@
 #include "simulation.hpp"
 
 void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)> rf){
-    for(Time t = 1; t == d+1; t++){
+    for(Time t = 1; t <= d; t++){
         if (!f.is_consistent()){ throw std::logic_error("Fabryka nie jest spójna");}
         f.do_deliveries(t);
-        f.do_work(t);
         f.do_package_passing();
+        f.do_work(t);
         rf(f,t);
     }
 }
